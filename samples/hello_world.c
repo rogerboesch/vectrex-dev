cce3bc3ae40935a3e7eb37b1e11ce3a7a467d7dd
Roger Boesch <roger@captural.com>
2020-11-27 10:18:16 +0100

    Update samples

--- samples/hello_world.c
+++ samples/hello_world.c
@@ -1,5 +1,11 @@
 #include <vectrex/bios.h>
 
+#pragma vx_copyright "2020"
+#pragma vx_title_pos 0,-80
+#pragma vx_title_size -8, 80
+#pragma vx_title "g CLASSICS CODER"
+#pragma vx_music vx_music_1
+
 int main() {
   while(1) {
     wait_retrace();
---
+++ samples/moving_cube.c
@@ -0,0 +1,38 @@
+#include <vectrex/bios.h>
+
+#pragma vx_copyright "2020"
+#pragma vx_title_pos 0,-80
+#pragma vx_title_size -8, 80
+#pragma vx_title "g CLASSICS CODER"
+#pragma vx_music vx_music_1
+
+int8_t rectangle[8] = {
+	40, 0,
+	0, 40,
+	-40, 0,
+	0, -40
+};
+
+int main() {
+	int8_t x = -20;
+	int8_t y = -20;
+
+    while(1) {
+        wait_recal();
+    
+		controller_enable_1_x();
+		controller_enable_1_y();
+    		controller_check_joysticks();
+
+		if (controller_joystick_1_x() < 0) y -= 1;
+		if (controller_joystick_1_x() > 0) y += 1;
+		if (controller_joystick_1_y() < 0) x -= 1;
+		if (controller_joystick_1_y() > 0) x += 1;
+
+        intensity_a(0x7f);
+        moveto_d(x, y);
+        draw_vl_a(4, rectangle);
+    }
+
+    return 0;
+}
