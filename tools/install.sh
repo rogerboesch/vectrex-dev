8834808540d1e8b53460d6519530d6b4f348c293
Roger <me@rogerboesch.com>
2020-11-28 14:21:06 +0100

    Update install.sh

--- tools/install.sh
+++ tools/install.sh
@@ -9,7 +9,7 @@ make install
 #
 # Download & install gcc6809 style version used by Classics Coder
 #
-curl -O https://raw.githubusercontent.com/rogerboesch/vectrex-dev/master/tools/cmoc-classics-coder.tar.gz
+curl -O https://raw.githubusercontent.com/rogerboesch/vectrex-dev/master/tools/Classics_Coder_for_Vectrex-Latest.tar.gz
 tar zxvf cmoc-classics-coder.tar.gz
 cd cmoc-classics-coder
 #
