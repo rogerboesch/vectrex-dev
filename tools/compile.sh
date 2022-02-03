0731138a82c15783093a6f09e54d391975b73fb7
Roger <me@rogerboesch.com>
2022-02-03 15:37:03 +0100

    Update compile.sh

--- tools/compile.sh
+++ tools/compile.sh
@@ -6,4 +6,4 @@ export PATH="$HOME/usr/bin:$HOME/usr/local/bin:$PATH"
 #
 # Call cmoc
 #
-cmoc -I$HOME/usr/share/cmoc/include -L$HOME/usr/share/cmoc/lib --vectrex --verbose -o $1.bin $1.c
+cmoc -I$HOME/usr/local/share/cmoc/include -L$HOME/usr/local/share/cmoc/lib --vectrex --verbose -o $1.bin $1.c
