452bb2417cebbd7080f628502308a4ce41612d44
Roger <me@rogerboesch.com>
2022-02-03 11:17:09 +0100

    Update install.sh
    USe local path to avoid permission issues

--- tools/install.sh
+++ tools/install.sh
@@ -1,6 +1,11 @@
+#
+# Use user path to avoid permission issues
 # 
+export DESTDIR="$HOME"
+export PATH="$HOME/usr/bin:$HOME/usr/local/bin:$PATH"
+#
 # Download & install lwtools
-# 
+#
 curl -O http://www.lwtools.ca/releases/lwtools/lwtools-4.17.tar.gz
 tar zxvf lwtools-4.17.tar.gz
 cd lwtools-4.17
@@ -16,9 +21,9 @@ cd cmoc-classics-coder
 #
 # Standard version
 #
-# curl -O http://perso.b2b2c.ca/~sarrazip/dev/cmoc-0.1.67.tar.gz
-# tar zxvf cmoc-0.1.67.tar.gz
-# cd cmoc-0.1.67
+#curl -O http://perso.b2b2c.ca/~sarrazip/dev/cmoc-0.1.67.tar.gz
+#tar zxvf cmoc-0.1.67.tar.gz
+#cd cmoc-0.1.67
 #
 ./autogen.sh
 ./configure
