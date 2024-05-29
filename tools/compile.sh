a114bb31289add785edfcd18538e618a775286df
= <mail@rogerboesch.com>
2024-05-29 14:09:43 +0200

    Update scripts

--- tools/compile.sh
+++ tools/compile.sh
@@ -3,4 +3,4 @@
 # - Parameter $1: Name of file
 # - Parameter $2: Path to CMOC
 #
-cmoc -I$2/stdlib -L$2/stdlib --vectrex --verbose -o $1.bin $1.c
+$2/cmoc -I$2/stdlib -L$2/stdlib --vectrex --verbose -o $1.bin $1.c
