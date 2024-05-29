4cc281d99a00c6090b4a91b3959318795cf6a0c6
= <mail@rogerboesch.com>
2024-05-29 12:21:46 +0200

    Updates for VectreC

--- tools/README.md
+++ tools/README.md
@@ -1,7 +1,9 @@
 # Vectrex C Development
 
+I'm currently working on an update which should be available soon. Thanks for your understanding!
+
 ### Files
 
-- **Classics Coder for Vectrex**, latest version: *Classics_Coder_for_Vectrex-Latest.tar.gz (IDE for use with CMOC C-Compiler)*
-- **Improved** CMOC version: *cmoc-classics-coder.tar.gz (used in Classics Coder for Vectrex App)*
+- **VectreC**, latest version: *Classics_Coder_for_Vectrex-Latest.tar.gz (IDE for use with CMOC C-Compiler)*
+- **Improved** CMOC version: *cmoc-vectrec.tar.gz (used in VectreC App)*
 - Installation script: *install.sh (Can also be used to install CMOC only)*
--- tools/install.sh
+++ tools/install.sh
@@ -1,33 +1,39 @@
 #
 # Use user path to avoid permission issues
 # 
-export DESTDIR="$HOME"
-export PATH="$HOME/usr/bin:$HOME/usr/local/bin:$PATH"
+export INSTALLDIR="$HOME/cmoc-test"
+export PATH="$INSTALLDIR:$PATH"
 #
-# Download & install lwtools
+# Download & build lwtools
 #
 curl -O http://www.lwtools.ca/releases/lwtools/lwtools-4.17.tar.gz
 tar zxvf lwtools-4.17.tar.gz
 cd lwtools-4.17
 make
-make install
+#
+# Copy binaries to install path
+#
+cp lwar/lwar $INSTALLDIR/lwar
+cp lwasm/lwasm $INSTALLDIR/lwasm
 cd ..
 #
-# Download & install gcc6809 style version used by Classics Coder
+# Download & build gcc6809 style version used by VectreC
 #
-curl -O https://raw.githubusercontent.com/rogerboesch/vectrex-dev/master/tools/cmoc-classics-coder.tar.gz
-tar zxvf cmoc-classics-coder.tar.gz
-cd cmoc-classics-coder
-#
-# Standard version
-#
-#curl -O http://perso.b2b2c.ca/~sarrazip/dev/cmoc-0.1.67.tar.gz
-#tar zxvf cmoc-0.1.67.tar.gz
-#cd cmoc-0.1.67
-#
+curl -O https://raw.githubusercontent.com/rogerboesch/vectrex-dev/master/tools/cmoc-vectrec.tar.gz
+tar zxvf cmoc-vectrec.tar.gz
+cd cmoc-vectrec
 ./autogen.sh
-./configure --prefix="usr"
+./configure
 make
-make install
+#
+# Copy binaries to install path
+#
+cp src/cmoc $INSTALLDIR/cmoc
+#
+# Finished
+#
+echo "VectreC installed."
+echo ""
+#
 cd ..
 cd ..
